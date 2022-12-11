923a624178d0211127aa2d66fafa2357df754019
ahmedmahovac <amahovac1@etf.unsa.ba>
2022-12-11 18:14:19 +0100

    napravljen update

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,18 +1,23 @@
 const PostModel = require('../models/post')
 const CommentModel = require('../models/comment')
 
+const slug = require("limax");
+
 exports.getPost = (req,res) => {
  
 }
 
 exports.createPost = (req,res) => {
     // napravi i za to kad nema required polja
+    // sta ako vec postoji isti slug
+    // samo zabranit kreiranje ako vec postoji isti naslov
     const {title, description, body, tagList} = req.body.blogPost;
-    console.log(req.body);
+    const postSlug = slug(title);
+    console.log(postSlug);
     if(tagList==null){ // ako uopste nije poslano 
         tagList = []
     }
-    PostModel.create({title: title, description: description, body: body, tagList: tagList}, (err, createdPost) => {
+    PostModel.create({slug: postSlug, title: title, description: description, body: body, tagList: tagList}, (err, createdPost) => {
         if(err){
             res.sendStatus(500);
         }
@@ -21,7 +26,18 @@ exports.createPost = (req,res) => {
 }
 
 exports.updatePost = (req,res) => {
-    
+    const {title} = req.body.blogPost;
+    const postSlug = slug(title);
+    // opcionalna polja jsona, vidjet sta za to
+    PostModel.findOneAndUpdate({slug: req.params.slug}, {slug: postSlug, title: title}, (err,data)=>{
+        console.log(data);
+        if(err){
+            res.sendStatus(500);
+        }
+        else {
+            res.json(data);
+        }
+    });
 }
 
 exports.deletePost = (req,res) => {
