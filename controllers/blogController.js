0f98ccb7c720515e4cf5d4163e359e4f13b9d39f
ahmedmahovac <amahovac1@etf.unsa.ba>
2022-12-11 06:37:08 +0100

    added route for creating blog post

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,13 +1,23 @@
 const PostModel = require('../models/post')
 const CommentModel = require('../models/comment')
-const TagModel = require('../models/tag')
 
 exports.getPost = (req,res) => {
  
 }
 
 exports.createPost = (req,res) => {
-
+    // napravi i za to kad nema required polja
+    const {title, description, body, tagList} = req.body.blogPost;
+    console.log(req.body);
+    if(tagList==null){ // ako uopste nije poslano 
+        tagList = []
+    }
+    PostModel.create({title: title, description: description, body: body, tagList: tagList}, (err, createdPost) => {
+        if(err){
+            res.sendStatus(500);
+        }
+        else res.json(createdPost);
+    })
 }
 
 exports.updatePost = (req,res) => {
@@ -31,5 +41,5 @@ exports.getComments = (req,res) => {
 }
 
 exports.deleteComment = (req,res) => {
-    
+
 }
